de937ea080a10ae9831f9ac9714017d5a34aa8b1
hoktayerkol <hokayerkol@hotmail.com>
2023-11-29 11:36:07 +0300

    comments added

--- pp_tut/src/mtc_tutorial.cpp
+++ pp_tut/src/mtc_tutorial.cpp
@@ -167,21 +167,35 @@ mtc::Task MTCTaskNode::createTask()
   stage_open_hand->setGoal("open");
   task.add(std::move(stage_open_hand));
 
+  // We need to move the arm to a position where we can pick up our object. This is done with 
+  // a Connect stage, which as its name implies, is a Connector stage. This means that it tries 
+  // to bridge between the results of the stage before and after it.
   auto stage_move_to_pick = std::make_unique<mtc::stages::Connect>("move to pick",
-                            mtc::stages::Connect::GroupPlannerVector{ {arm_group_name, sampling_planner}});
+                            mtc::stages::Connect::GroupPlannerVector{ {arm_group_name, sampling_planner} });
   stage_move_to_pick->setTimeout(5.0);
-  stage_move_to_pick->properties().configureInitFrom(mtc::Stage::PARENT);
+  stage_move_to_pick->properties().configureInitFrom(mtc::Stage::PARENT); // set the properties for the stage
   task.add(std::move(stage_move_to_pick));
 
+  // Next, we create a pointer to a MoveIt Task Constructor stage object, and set it to nullptr for now. 
+  // Later, we will use this to save a stage.
   mtc::Stage* attach_object_stage = nullptr;  // Forward attach_object_stage to place pose generator
 
+  // This next block of code creates a SerialContainer. 
+  // This is a container that can be added to our task and can hold several substages. 
+  // In this case, we create a serial container that will contain the stages relevant to 
+  // the picking action. Instead of adding the stages to the task, we will add the relevant stages to the serial container.
   {
   auto grasp = std::make_unique<mtc::SerialContainer>("pick object");
+  // We use exposeTo to declare the task properties from the parent task and use configureInitFrom() to initialize them. 
   task.properties().exposeTo(grasp->properties(), { "eef", "group", "ik_frame" });
   grasp->properties().configureInitFrom(mtc::Stage::PARENT,
                                         { "eef", "group", "ik_frame" });
                                 
     {
+      // This stage is a MoveRelative stage, which allows us to specify a relative movement from our current position.
+      // MoveRelative is a propagator stage: it receives the solution from its neighbouring stages and propagates it 
+      // to the next or previous stage. Using cartesian_planner finds a solution that involves moving the end effector 
+      // in a straight line. We set the properties, and set the minimum and maximum distance to move. 
       auto stage =
           std::make_unique<mtc::stages::MoveRelative>("approach object", cartesian_planner);
       stage->properties().set("marker_ns", "approach_object");
@@ -189,6 +203,8 @@ mtc::Task MTCTaskNode::createTask()
       stage->properties().configureInitFrom(mtc::Stage::PARENT, { "group" });
       stage->setMinMaxDistance(0.1, 0.15);
 
+      // create a Vector3Stamped message to indicate the direction we want to move - in this case, in the Z direction 
+      // from the hand frame. Finally, we add this stage to our serial container
       // Set hand forward direction
       geometry_msgs::msg::Vector3Stamped vec;
       vec.header.frame_id = hand_frame;
@@ -196,17 +212,31 @@ mtc::Task MTCTaskNode::createTask()
       stage->setDirection(vec);
       grasp->insert(std::move(stage));
     }
+
     {
+      // create a stage to generate the grasp pose. This is a generator stage, so it computes 
+      // its results without regard to the stages before and after it.
+      // The first stage, CurrentState is a generator stage as well - to connect the first stage and 
+      // this stage, a connecting stage must be used, which we already created above.
       // Sample grasp pose
       auto stage = std::make_unique<mtc::stages::GenerateGraspPose>("generate grasp pose");
       stage->properties().configureInitFrom(mtc::Stage::PARENT);
       stage->properties().set("marker_ns", "grasp_pose");
-      stage->setPreGraspPose("open");
+      stage->setPreGraspPose("open"); // pose before graping
       stage->setObject("object");
+      // Angle delta is a property of the GenerateGraspPose stage that is used to determine 
+      // the number of poses to generate; when generating solutions, MoveIt Task Constructor will try to grasp 
+      // the object from many different orientations, with the difference between the orientations specified by the angle delta.
       stage->setAngleDelta(M_PI / 12);
+      // When defining the current stage, we set current_state_ptr, which is now used to forward information about 
+      // the object pose and shape to the inverse kinematic solver. 
       stage->setMonitoredStage(current_state_ptr);  // Hook into current state
+      // This stage won’t be directly added to the serial container like previously, as we still need to do 
+      // inverse kinematics on the poses it generates.
 
-      // 
+      // Before we compute inverse kinematics for the poses generated above, we first need to define the frame. 
+      // This can be done with a PoseStamped message from geometry_msgs or in this case, we define the transform 
+      // using Eigen transformation matrix and the name of the relevant link.
       Eigen::Isometry3d grasp_frame_transform;
       Eigen::Quaterniond q = Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitX()) *
                               Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitY()) *
@@ -214,7 +244,11 @@ mtc::Task MTCTaskNode::createTask()
       grasp_frame_transform.linear() = q.matrix();
       grasp_frame_transform.translation().z() = 0.1;
 
-      // Compute IK
+      // Now, we create the ComputeIK stage, and give it the name generate pose IK as well as the generate 
+      // grasp pose stage defined above. Some robots have multiple inverse kinematics solutions for 
+      // a given pose - we set the limit on the amount of solutions to solve for up to 8. We also set the minimum 
+      // solution distance, which is a threshold on how different solutions must be: if the joint positions in a solution 
+      // are too similar to a previous solution, it will be marked as invalid.
       auto wrapper =
           std::make_unique<mtc::stages::ComputeIK>("grasp pose IK", std::move(stage));
       wrapper->setMaxIKSolutions(8);
@@ -225,6 +259,10 @@ mtc::Task MTCTaskNode::createTask()
       grasp->insert(std::move(wrapper));
     }
 
+    // In order to pick up the object, we must allow collision between the hand and the object. This can be done with 
+    // a ModifyPlanningScene stage. The allowCollisions function lets us specify which collisions to disable. allowCollisions 
+    // can be used with a container of names, so we can use getLinkModelNamesWithCollisionGeometry to get all the names 
+    // of links with collision geometry in the hand group.
     {
       auto stage =
           std::make_unique<mtc::stages::ModifyPlanningScene>("allow collision (hand,object)");
@@ -236,6 +274,8 @@ mtc::Task MTCTaskNode::createTask()
       grasp->insert(std::move(stage));
     }
 
+    // With collisions allowed, we now can close the hand. This is done with a MoveTo stage, similarly to the open hand stage 
+    // from above, except moving to the close position as defined in the SRDF.
     {
       auto stage = std::make_unique<mtc::stages::MoveTo>("close hand", interpolation_planner);
       stage->setGroup(hand_group_name);
@@ -243,6 +283,9 @@ mtc::Task MTCTaskNode::createTask()
       grasp->insert(std::move(stage));
     }
 
+    // We now use a ModifyPlanningScene stage again, this time to attach the object to the hand using attachObject. 
+    // Similarly to what we did with the current_state_ptr, we get a pointer to this stage for later use when generating 
+    // the place pose for the object.
     {
       auto stage = std::make_unique<mtc::stages::ModifyPlanningScene>("attach object");
       stage->attachObject("object", hand_frame);
@@ -250,6 +293,7 @@ mtc::Task MTCTaskNode::createTask()
       grasp->insert(std::move(stage));
     }
 
+    // Next, we lift the object with a MoveRelative stage, similarly to the approach_object stage.
     {
       auto stage =
           std::make_unique<mtc::stages::MoveRelative>("lift object", cartesian_planner);
@@ -270,6 +314,9 @@ mtc::Task MTCTaskNode::createTask()
   }
 
   // Place Stages
+
+  // We start with a Connect stage to connect the two, as we will soon be using a generator stage 
+  // to generate the pose for placing the object.
   {
     auto stage_move_to_place = std::make_unique<mtc::stages::Connect>(
         "move to place",
@@ -279,6 +326,7 @@ mtc::Task MTCTaskNode::createTask()
     stage_move_to_place->properties().configureInitFrom(mtc::Stage::PARENT);
     task.add(std::move(stage_move_to_place));
   }
+
   // We also create a serial container for the place stages.
   {
     auto place = std::make_unique<mtc::SerialContainer>("place object");
@@ -286,8 +334,11 @@ mtc::Task MTCTaskNode::createTask()
     place->properties().configureInitFrom(mtc::Stage::PARENT,
                                           { "eef", "group", "ik_frame" });
     
+    // This next stage generates the poses used to place the object and compute the inverse kinematics 
+    // for those poses - it is somewhat similar to the generate grasp pose stage from the pick serial container. 
     {
       // Sample place pose
+      // We start by creating a stage to generate the poses and inheriting the task properties.
       auto stage = std::make_unique<mtc::stages::GeneratePlacePose>("generate place pose");
       stage->properties().configureInitFrom(mtc::Stage::PARENT);
       stage->properties().set("marker_ns", "place_pose");
@@ -298,9 +349,13 @@ mtc::Task MTCTaskNode::createTask()
       target_pose_msg.pose.position.y = 0.5;
       target_pose_msg.pose.orientation.w = 1.0;
       stage->setPose(target_pose_msg);
+      // we use setMonitoredStage and pass it the pointer to the attach object stage from earlier. 
+      // This allows the stage to know how the object is attached. 
       stage->setMonitoredStage(attach_object_stage);  // Hook into attach_object_stage
 
       // Compute IK
+      // We then create a ComputeIK stage and pass it our GeneratePlacePose stage - the rest follows 
+      // the same logic as above with the pick stages.
       auto wrapper =
           std::make_unique<mtc::stages::ComputeIK>("place pose IK", std::move(stage));
       wrapper->setMaxIKSolutions(2);
@@ -311,7 +366,7 @@ mtc::Task MTCTaskNode::createTask()
       place->insert(std::move(wrapper));
     }
 
-    // Now that we’re ready to place the object
+    // Now that we’re ready to place the object, we open the hand with MoveTo stage and the joint interpolation planner.
     {
       auto stage = std::make_unique<mtc::stages::MoveTo>("open hand", interpolation_planner);
       stage->setGroup(hand_group_name);
@@ -364,7 +419,7 @@ mtc::Task MTCTaskNode::createTask()
   {
     auto stage = std::make_unique<mtc::stages::MoveTo>("return home", interpolation_planner);
     stage->properties().configureInitFrom(mtc::Stage::PARENT, { "group" });
-    stage->setGoal("ready");
+    stage->setGoal("ready");  // which is a pose defined in the panda SRDF
     task.add(std::move(stage));
   }
 
